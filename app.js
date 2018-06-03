99248bcb33b5b72ea5243d050c4fc492bfc18b6c
Francesco Pongetti <fpongetti@fpongetti-macbookpro1.roam.corp.google.com>
2018-06-03 18:59:27 +0100

    Move to biquery, new tweets format, new tweet filtering.

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const Twitter = require('twitter');
 const config = require('./config.json');
 
 const language_client = new language.LanguageServiceClient({
-  keyFilename: './datastore.json'
+  keyFilename: './key.json'
 });
 
 const bigquery = new BigQuery({
   projectId: config.project_id,
-  keyFilename: './datastore.json'
+  keyFilename: './key.json'
 });
 const table = bigquery
   .dataset(config.bigquery_dataset)
@@ -23,19 +23,34 @@ const client = new Twitter({
   access_token_secret: config.twitter_access_secret
 });
 
-client.stream('statuses/filter', {track: config.searchTerms, language: 'it'}, function(stream) {
-  stream.on('data', function(event) {
-    processTweet(event);
-  });
-  stream.on('ERROR', function(error) {
-    console.log('Twitter api error: ' + error)
-  });
-});
+const allFollows = [...config.followLega,
+    ...config.followM5S,
+    ...config.followPD,
+    ...config.followForzaItalia,
+    ...config.followManganelliDitalia];
 
-function processTweet(event) {
-  if ((event.text != undefined) && (event.text.substring(0,2) != 'RT')) {
-    analyzeTweet(event);
-  }
+const allIdsRequests = allFollows.map(async (item) => {
+       response = await client.get('users/show', {screen_name:item});
+       return response.id;
+     });
+
+Promise.all(allIdsRequests)
+  .then(ids => {
+    const filter = ids.join(',');
+    client.stream('statuses/filter', {follow: filter, language: 'it'}, function(stream) {
+      stream.on('data', function(event) {
+        processTweet(ids, event);
+      });
+      stream.on('ERROR', function(error) {
+        console.log('Twitter api error: ' + error)
+      });
+    });
+  });
+
+
+function processTweet(ids, event) {
+  if ((event.text == undefined) || (event.text.substring(0,2) == 'RT')) return;
+  analyzeTweet(event);
 }
 
 function analyzeTweet(event) {
@@ -49,11 +64,15 @@ function analyzeTweet(event) {
       const entity = results[0].entities.sort(function(a, b) {
         return b.salience - a.salience;
       })[0];
-
       const tweet = {
-        created_at: event.created_at,
+        created_ms: Math.round(event.timestamp_ms / 1000),
         id: event.id,
         text: event.text,
+        in_reply_to_screen_name: event.in_reply_to_screen_name,
+        screen_name: event.user.screen_name,
+        retweet_count: event.retweet_count,
+        reply_count: event.reply_count,
+        favorite_count: event.favorite_count,
         sentiment_score: sentiment.score || 0,
         sentiment_magnitude: sentiment.magnitude || 0,
         entity: entity.name,
@@ -73,10 +92,13 @@ function saveTask(tweet) {
   table.insert(tweet)
   .then(function(data) {
     var apiResponse = data[0];
-    console.log(apiResponse);
     console.log(tweet);
   })
   .catch(function(err) {
+    if (err.name === 'PartialFailureError') {
+      console.log(err.errors[0].errors)
+    }
+    console.log('ERRORE');
     console.log(err);
   });
 }
